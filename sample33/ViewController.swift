d914d2f7cc398ce0a59c34cafbbf32146fd0e745
monish <monishraja0200@gmail.com>
2022-01-06 10:45:23 +0530

    Initial Commit

---
+++ sample33/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sample33
+//
+//  Created by monish-pt4670 on 16/10/1943 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
